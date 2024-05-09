7ace511c4f27f2ab0e725aa66606ac71a054a7e3
Mohana Krishna Tellakula <tellakulamk@gmail.com>
2024-05-09 10:40:39 +0530

    Added the LogOut Navigation and modification of value

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -6,18 +6,28 @@ import ImportContactsIcon from '@mui/icons-material/ImportContacts';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { UseSelector, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, useAppSelector } from '@/redux/store';
-import { toggleLogOutStatus } from '@/redux/features/auth-slice';
+import { toggleLogOutStatus } from '@/redux/features/auth-slice'; 
+import paths from './path';
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
  // const [isLoggedIn, setIsLoggedIn] = useState(false);
   //const [userName, setUserName] = useState("Mohana Krishna");
-  const dispatch = useDispatch<AppDispatch>();
-  const userName= useAppSelector((state) => state.authReducer.value.username);
-  const isLoggedIn= useAppSelector((state) => state.authReducer.value.isLoginStatus); 
-  console.log("--test-------", isLoggedIn)
+  const dispatch = useDispatch<AppDispatch>(); 
+  const router = useRouter();
+  // const userName= useAppSelector((state) => state.authReducer.value.username);
+  // const isLoggedIn= useAppSelector((state) => state.authReducer.value.isLoginStatus); 
+  const loginDetails = useAppSelector((state) => ({
+    userName: state.authReducer.value.username,
+    isLoginStatus: state.authReducer.value.isLoginStatus
+  }));
+  
+  console.log("--test-------", loginDetails)
   const handleLogout = () => {
     // Perform logout actions here, such as clearing local storage, resetting state, etc.
-    dispatch(toggleLogOutStatus())
+    console.log("---set------",paths.signIn())
+    router.push(paths.signIn());  
+    dispatch(toggleLogOutStatus());
    // setUserName("");
   };
   return (
@@ -26,10 +36,10 @@ const Header = () => {
       <Typography variant="body2" align="right" sx={{ color: "#00ff00", fontWeight: "bold", fontSize: "20px" }}>
         <ImportContactsIcon sx={{ color: "#00ff00", fontSize: "50px"  }} /> <span style={{ color: "#00ff00", fontSize: "30px"}}>Knowledge</span> <span style={{ color: "#ffffff", fontWeight: "bold", textShadow: "1px 1px 2px black" , fontSize: "30px" }}>Hub</span>
       </Typography>
-      {isLoggedIn && (
+      {loginDetails.isLoginStatus && (
           <div style={{ marginLeft: 'auto', display: 'flex', alignItems: 'center' }}>
             <Typography variant="body2" align="right" sx={{ color: "#ffffff", fontWeight: "bold", fontSize: "16px" }}>
-              {userName}
+              {loginDetails.userName}
             </Typography>
             <IconButton color="inherit" onClick={handleLogout}>
               <LogoutIcon />
