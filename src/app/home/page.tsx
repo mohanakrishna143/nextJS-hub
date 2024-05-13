a2cf5b6e8f7b468d12095e6ec8a09e4888b09408
Mohana Krishna Tellakula <tellakulamk@gmail.com>
2024-05-13 23:11:05 +0530

    Added image and Tiles and icon change

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,24 +1,25 @@
 "use client"
-import React from "react";
+import React, { useState } from "react";
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Avatar, TablePagination, IconButton, Typography, Divider } from '@mui/material';
-import { GridOn as GridIcon } from '@mui/icons-material';
-import ViewCompactIcon from '@mui/icons-material/ViewCompact';
+import { GridOn as GridIcon, ViewCompact as ViewCompactIcon } from '@mui/icons-material';
+import TocIcon from '@mui/icons-material/Toc';
 
 // Dummy data for demonstration
 const dummyData = [
-  { id: 1, firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', dateOfJoining: '2022-01-01', imageUrl: '' },
-  { id: 2, firstName: 'Geetha', lastName: 'Krishna', email: 'gee.kri@example.com', dateOfJoining: '2022-02-01', imageUrl: null },
-  { id: 3, firstName: 'Jane', lastName: 'Smith', email: 'jane.smith@example.com', dateOfJoining: '2022-02-01', imageUrl: null },
-  { id: 4, firstName: 'Jane', lastName: 'Smith', email: 'jane.smith@example.com', dateOfJoining: '2022-02-01', imageUrl: null },
-  { id: 5, firstName: 'Jane', lastName: 'Smith', email: 'jane.smith@example.com', dateOfJoining: '2022-02-01', imageUrl: null },
-  { id: 6, firstName: 'Jane', lastName: 'Smith', email: 'jane.smith@example.com', dateOfJoining: '2022-02-01', imageUrl: null }
+  { id: 1, firstName: 'Mohana', lastName: 'Krishna', email: 'john.doe@example.com', dateOfJoining: '2022-01-01', imageUrl: '/01.jpg' },
+  { id: 2, firstName: 'Narayana', lastName: 'Akkili', email: 'gee.kri@example.com', dateOfJoining: '2022-02-01', imageUrl: '/02.jpg' },
+  { id: 3, firstName: 'Madhu', lastName: 'RK', email: 'jane.smith@example.com', dateOfJoining: '2022-02-01', imageUrl: null },
+  { id: 4, firstName: 'Ravi', lastName: 'RK', email: 'jane.smith@example.com', dateOfJoining: '2022-02-01', imageUrl: null },
+  { id: 5, firstName: 'Syam', lastName: 'Turi', email: 'jane.smith@example.com', dateOfJoining: '2022-02-01', imageUrl: null },
+  { id: 6, firstName: 'Sharan', lastName: 'Teja', email: 'jane.smith@example.com', dateOfJoining: '2022-02-01', imageUrl: null }
   // Add more dummy data as needed
 ];
 
 function Home() {
   // Pagination state
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [gridView, setGridView] = useState(false); // State to toggle between table and grid view
 
   // Change page handler
   const handleChangePage = (event: any, newPage: React.SetStateAction<number>) => {
@@ -31,47 +32,78 @@ function Home() {
     setPage(0);
   };
 
+  // Toggle between table and grid view
+  const toggleView = () => {
+    setGridView(!gridView);
+  };
+
   return (
-    <div style={{ marginTop: "150px", padding: "0 20px", marginBottom: "20px" }}>
+    <div style={{ marginTop: "150px", padding: "0 20px", marginBottom: "20px", position: 'relative' }}>
       <Typography variant="h4" gutterBottom>
         Employee 
       </Typography>
-      <Divider />
-      <ViewCompactIcon />
-      <IconButton style={{ position: 'absolute', top: 10, right: 0 }}>
-        <GridIcon />
+      <Divider /> 
+      <IconButton style={{ position: 'absolute', top: 10, right: '1%', }}>
+        {!gridView && <ViewCompactIcon onClick={toggleView} fontSize="large" />}
+        {gridView &&<TocIcon onClick={toggleView} fontSize="large"/>}
       </IconButton>
-      <TableContainer component={Paper} style={{ padding: "20px" }}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell style={{ backgroundColor: '#FFD580', height: '30px' }}>Image</TableCell>
-              <TableCell style={{ backgroundColor: '#FFD580', height: '30px' }}>Last Name</TableCell>
-              <TableCell style={{ backgroundColor: '#FFD580', height: '30px' }}>First Name</TableCell>
-              <TableCell style={{ backgroundColor: '#FFD580', height: '30px' }}>Email ID</TableCell>
-              <TableCell style={{ backgroundColor: '#FFD580', height: '30px' }}>Date of Joining</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {dummyData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-              <TableRow key={row.id}  >
-                <TableCell>
-                  {row.imageUrl ? (
-                    <Avatar alt={`${row.firstName} ${row.lastName}`} src={row.imageUrl} />
-                  ) : (
-                    <Avatar>{`${row.firstName.charAt(0)}${row.lastName.charAt(0)}`}</Avatar>
-                  )}
-                </TableCell>
-                <TableCell>{row.lastName}</TableCell>
-                <TableCell>{row.firstName}</TableCell>
-                <TableCell>{row.email}</TableCell>
-                <TableCell>{row.dateOfJoining}</TableCell>
+      {gridView ? (
+        // Grid view
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
+          {dummyData.map((employee) => (
+            <div key={employee.id} style={{ position: 'relative' }}>
+               {employee.imageUrl ? (
+                      <Avatar alt={`${employee.firstName} ${employee.lastName}`} 
+                      src={employee.imageUrl} 
+                      style={{ cursor: 'pointer', marginTop:"20px" }} 
+                      title={`${employee.firstName} ${employee.lastName}`} />
+                    ) : (
+                      <Avatar style={{ cursor: 'pointer', marginTop:"20px" }} 
+                      title={`${employee.firstName} ${employee.lastName}`} >{`${employee.firstName.charAt(0)}${employee.lastName.charAt(0)}`}</Avatar>
+                    )}
+            
+            <Typography 
+              variant="body2" 
+              style={{ position: 'absolute', bottom: '-20px', left: '50%', transform: 'translateX(-50%)', width: '100%', textAlign: 'center' }}
+            >
+               
+            </Typography>
+          </div>
+          
+          ))}
+        </div>
+      ) : (
+        // Table view
+        <TableContainer component={Paper} style={{ padding: "20px" }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ backgroundColor: '#FFD580', height: '30px' }}>Image</TableCell>
+                <TableCell style={{ backgroundColor: '#FFD580', height: '30px' }}>Last Name</TableCell>
+                <TableCell style={{ backgroundColor: '#FFD580', height: '30px' }}>First Name</TableCell>
+                <TableCell style={{ backgroundColor: '#FFD580', height: '30px' }}>Email ID</TableCell>
+                <TableCell style={{ backgroundColor: '#FFD580', height: '30px' }}>Date of Joining</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
+            </TableHead>
+            <TableBody>
+              {dummyData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell>
+                    {row.imageUrl ? (
+                      <Avatar alt={`${row.firstName} ${row.lastName}`} src={row.imageUrl} />
+                    ) : (
+                      <Avatar>{`${row.firstName.charAt(0)}${row.lastName.charAt(0)}`}</Avatar>
+                    )}
+                  </TableCell>
+                  <TableCell>{row.lastName}</TableCell>
+                  <TableCell>{row.firstName}</TableCell>
+                  <TableCell>{row.email}</TableCell>
+                  <TableCell>{row.dateOfJoining}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <TablePagination
         rowsPerPageOptions={[5, 10, 25]}
         component="div"
         count={dummyData.length}
@@ -80,6 +112,10 @@ function Home() {
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
       />
+        </TableContainer>
+        
+      )}
+      
     </div>
   );
 }
